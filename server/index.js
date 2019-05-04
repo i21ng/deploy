7c69172939e5889c7e57f7e7b4c7a9b141f53a95
i21ng <garcia.isisnayely@gmail.com>
2019-05-04 14:17:49 -0500

    configs

--- server/index.js
+++ server/index.js
@@ -11,4 +11,12 @@ mongoose.connect(keys.MONGO_URL, { useNewUrlParser: true });
 
 require('./routes/animalesRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    });
+}
+
 app.listen(process.env.PORT || 5000);
